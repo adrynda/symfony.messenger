d665c6595f48c05c953cdee12a6955c0a4433b72
olek <olekdrynda@gmail.com>
2026-06-28 12:56:21 +0200

    poprawa obrazu

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
-build-dev:
-	composer install
-	composer dump-env dev
-	php bin/console doctrine:database:drop --force --if-exists
-	php bin/console doctrine:database:create
-	php bin/console doctrine:migrations:migrate --no-interaction
-	php bin/console doctrine:fixtures:load --no-interaction
+build-%:
+	./_scripts/build.sh $*
+
+setup-%:
+	./_scripts/setup.sh $*
 
 build-front:
 	php bin/console cache:clear
---
+++ _scripts/setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+APP_ENV=${1:-dev}
+
+set -e
+
+(
+    cd _docker/$APP_ENV
+    docker compose exec -T app sh -c "
+        set -e
+        composer install
+        composer dump-env $APP_ENV
+        php bin/console doctrine:database:drop --force --if-exists
+        php bin/console doctrine:database:create
+        php bin/console doctrine:migrations:migrate --no-interaction
+        php bin/console doctrine:fixtures:load --no-interaction
+    "
+)
