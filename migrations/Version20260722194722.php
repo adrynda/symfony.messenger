97b3e469b0e9bd398afb3c9fe4084055c13990d7
Your Name <you@example.com>
2026-07-22 22:08:31 +0200

    dodanie "paczki" olek/audit

---
+++ migrations/Version20260722194722.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260722194722 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE audits (id UUID NOT NULL, username TEXT NOT NULL, diff JSON NOT NULL, entity_id VARCHAR(32) NOT NULL, entity_class VARCHAR(255) NOT NULL, action_type VARCHAR(6) NOT NULL, action_timestamp TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY (id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE audits');
+    }
+}
